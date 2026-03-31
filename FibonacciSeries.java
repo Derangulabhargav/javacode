efe8a019e11200c043ff9ae1ab36b911a9606402
unknown <bhargav2965@gmail.com>
2026-03-31 07:42:44 +0530

    Code updatated

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,5 +1,14 @@
 public class FibonacciSeries {
     public static void main(String[] args) {
-        System.out.print("Hi helo");
+        int num = 10;
+        int first = 0;
+        int sec = 1;
+        for (int i = 0; i <= num; i++) {
+            System.out.println(first);
+            int last = first + sec;
+            first = sec;
+            sec = last;
+        }
+
     }
 }
